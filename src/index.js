f5834a0cca5f4454642d1b90597c144039d7591f
David Phan <davidphan33@gmail.com>
2017-10-01 17:41:37 -0700

    web3-react wrraper

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM       from 'react-dom';
 import { Provider }   from 'react-redux';
 import configureStore from 'core/store/configureStore';
 import App            from 'containers/App';
+import { Web3Provider } from 'react-web3';
 
 const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App/>
+    <Web3Provider>
+      <App/>
+    </Web3Provider>
   </Provider>,
   document.getElementById('root')
 );
