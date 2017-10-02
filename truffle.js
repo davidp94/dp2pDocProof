4661b735f3ec81ad3f7f76939f562d9e73cdfc1f
David Phan <davidphan33@gmail.com>
2017-10-02 08:30:33 -0700

    added ropsten network migration + mnemonic sample

--- truffle.js
+++ truffle.js
@@ -1,3 +1,5 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = require('./credentials/mnemonic.js');
 module.exports = {
   migrations_directory: "./migrations",
   networks: {
@@ -5,6 +7,19 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, "http://localhost:8686"),
+      // port: 8686,
+      network_id: "3",
+      gas: 5000000,
+      gasPrice: 200000000000
+    }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
     }
   }
 };
